b163d3b7f31ca7182557620a4257dd57067d2b85
jihyun-00 <rkdwl811@naver.com>
2025-06-14 21:10:18 +0900

    20250610

---
+++ src/practice/one/Pcce1_5.java
@@ -0,0 +1,41 @@
+package practice.one;
+
+public class Pcce1_5 {
+	
+	public int[] solution(String route) {
+		int east = 0;
+		int north = 0;
+		int[] answer = new int [2];
+		for(int i=0; i<route.length(); i++){
+			switch(route.charAt(i)){
+			case 'N':
+				north++;
+				break;
+			case 'S':
+				
+				north--
+				;
+				break;
+			case 'E':
+				
+				east++
+				;
+				break;
+			case 'W':
+				
+				east--
+				;
+				
+				break
+				;
+			}
+		}
+		answer[0] = east;
+		answer[1] = north;
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		solution("NSSNEWWN");
+		}
+}
